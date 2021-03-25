709864618cb958e0d95903af20cd28b22b502b0d
Taeo Kim <kweras95@gmail.com>
2021-03-25 09:41:58 +0900

    Update MODELS configuration

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
-MODEL_NAME = "vgg16"
-MODEL_VERSION = 2
+MODEL_NAME = "inceptionv3"
+MODEL_VERSION = 1
 MODEL_VERSIONS = (1, 2)
 TARGET_SIZE = (224, 224)
+MODELS = {
+    "VGG16": {
+        "Name": "vgg16",
+        "ImportName": "vgg16",
+        "TargetSize": (224, 224),
+        "Versions": (1, 2)
+    },
+    "InceptionV3": {
+        "Name": "inceptionv3",
+        "ImportName": "inception_v3",
+        "TargetSize": (299, 299),
+        "Versions": (1, )
+    }
+}
 PORT = "8501"
